4761b05481308feaa0600b40bf90529879ea9508
soulnis <soulnis@naver.com>
2022-12-04 22:53:03 +0900

    삭제버튼 추가

--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import axios from "axios";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { withStyles } from "@mui/material/styles";
 
+
+const styles = theme =>({ hidden : {
+     display : 'none'
+}})
 
 class CustomerAdd extends React.Component {
 
@@ -12,7 +23,8 @@ class CustomerAdd extends React.Component {
             birthday: '',
             gender : '',
             job : '',
-            fileName : ''
+            fileName : '',
+            open : false
 
         }
      }
@@ -31,7 +43,8 @@ class CustomerAdd extends React.Component {
             birthday: '',
             gender : '',
             job : '',
-            fileName : ''
+            fileName : '',
+            open : false
 
         })
         
@@ -81,23 +94,64 @@ class CustomerAdd extends React.Component {
 
     }
 
+
+    handleClickOpen = () => {
+        this.setState({open:true})
+    }
+    handleClickClose = () => {
+        this.setState({   file:null,
+            userName : '',
+            birthday: '',
+            gender : '',
+            job : '',
+            fileName : '',
+            open : false })
+    }
+
      render () {
+
+        const {classes} = this.props;
         return (
             <div>
-            <form onSubmit={this.handleFormSubmit}>
-                <h1> 고객 추가</h1>
-                프로필 이미지 : <input type ='file' name ='file'  file={this.state.file} value ={this.state.fileName} onChange={this.handleFileChange}></input><br/>
-                이름 : <input type ='text' name ='userName'  value ={this.state.userName} onChange={this.handleValueChange}></input><br/>
-                생년월일 :  <input type ='text' name ='birthday'  value ={this.state.birthday} onChange={this.handleValueChange}></input><br/>
-                성별 :   <input type ='text' name ='gender'  value ={this.state.gender} onChange={this.handleValueChange}></input><br/>
-                직업 :  <input type ='text' name ='job'  value ={this.state.job} onChange={this.handleValueChange}></input><br/>
+            <Button variant='contained' color='primary' onClick={this.handleClickOpen} >고객 추가하기</Button>
+            <Dialog open={this.state.open} onClose={this.handleClickClose}>
+               <DialogTitle>고객추가</DialogTitle>
+                    <DialogContent>
+                        <input style ={{display:'none'}} accept="image/*" type='file' id="raised-button-file"  file={this.state.file} value ={this.state.fileName} onChange={this.handleFileChange}></input><br/> 
+                        <label htmlFor="raised-button-file">
+                            <Button  variant='contained' component='span' name='file'>
+                                {this.state.fileName === '' ?  '파일추가하기' : this.state.fileName}
+                            </Button>
+                        </label>
+                        <br/>
+                        <TextField label='이름' type ='text' name ='userName'  value ={this.state.userName} onChange={this.handleValueChange}></TextField><br/>
+                        <TextField label='생년월일' name ='birthday'  value ={this.state.birthday} onChange={this.handleValueChange}></TextField><br/>
+                        <TextField label='성별'name ='gender'  value ={this.state.gender} onChange={this.handleValueChange}></TextField><br/>
+                        <TextField label='직업' name ='job'  value ={this.state.job} onChange={this.handleValueChange}></TextField><br/>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant='contained'  color='primary' onClick={this.handleFormSubmit}  >추가</Button>
+                        <Button variant='outlined'  color='primary' onClick={this.handleClickClose} >닫기</Button>
 
-                <button type="submit"> 추가하기 </button>
-            </form>
+                    </DialogActions>
+            </Dialog>
+
             </div>
+            // <div>
+            // <form onSubmit={this.handleFormSubmit}>
+            //     <h1> 고객 추가</h1>
+            //     프로필 이미지 : <input type ='file' name ='file'  file={this.state.file} value ={this.state.fileName} onChange={this.handleFileChange}></input><br/>
+            //     이름 : <input type ='text' name ='userName'  value ={this.state.userName} onChange={this.handleValueChange}></input><br/>
+            //     생년월일 :  <input type ='text' name ='birthday'  value ={this.state.birthday} onChange={this.handleValueChange}></input><br/>
+            //     성별 :   <input type ='text' name ='gender'  value ={this.state.gender} onChange={this.handleValueChange}></input><br/>
+            //     직업 :  <input type ='text' name ='job'  value ={this.state.job} onChange={this.handleValueChange}></input><br/>
+
+            //     <button type="submit"> 추가하기 </button>
+            // </form>
+            // </div>
         )
      }
 
 }
 
-export default CustomerAdd;
+export default  CustomerAdd;
--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { withStyles } from "@mui/material/styles";
 
 class CustomerDelete extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+        
+            open : false
 
+        }
+     }
 
-    deleteCustomer(id){
+    deleteCustomer =(id)=>{
         const url ='/api/customers/' + id;
 
         fetch(url,{
@@ -14,9 +27,29 @@ class CustomerDelete extends React.Component {
         this.props.stateRefresh();
     }
 
+    handleClickOpen = () => {
+        this.setState({open:true})
+    }
+    handleClickClose = () => {
+        this.setState({   
+            open : false })
+    }
+
     render(){
         return (
-            <button onClick={ (e)=>{ this.deleteCustomer(this.props.id)}}>삭제하기</button>
+            <div>
+            <Button variant='contained' color='primary' onClick={this.handleClickOpen} >삭제하기</Button>  
+            <Dialog open={this.state.open} onClose={this.handleClickClose}>
+            <DialogTitle>삭제하기</DialogTitle>
+            <DialogContent>
+                <h3>정말로 데이터를 삭제하시겠습니까?</h3>
+            </DialogContent>
+              <DialogActions>
+                  <Button variant='contained' color='primary' onClick={(e)=>{this.deleteCustomer(this.props.id)}} > 삭제하기</Button>
+                  <Button variant='contained' color='primary' onClick={this.handleClickClose}> 취소하기</Button>
+              </DialogActions>
+            </Dialog>
+           </div>
         )
     }
 
